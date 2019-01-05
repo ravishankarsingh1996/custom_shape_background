603248e16f77a12cf2ccb0ac49187e3cf8ed7759
ravi <ravishankarsingh1996@gmail.com>
2019-01-06 01:23:16 +0530

    Custom shape painter.

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'theme/colors.dart';
+import 'top_bar.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Curved Path',
+      theme: appTheme,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: TopBar(),
+      ),
+    );
+  }
+}
+
---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Color colorOne = Colors.red;
+Color colorTwo = Colors.red[300];
+Color colorThree = Colors.red[100];
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.red,
+);
